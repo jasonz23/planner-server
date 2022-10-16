d9fef4a00f28b306fcf361acb467ba98ca284300
Jason Zhao <jason@goopter.com>
2022-10-16 00:37:12 -0700

    added routes

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,13 @@
 import express from 'express';
+
+import { getTasks, getTask, createTask, updateTask, deleteTask } from '../controllers/tasks.js';
+
+const router = express.Router();
+
+router.get('/', getTasks);
+router.post('/', createTask);
+router.get('/:id', getTask);
+router.patch('/:id', updateTask);
+router.delete('/:id', deleteTask);
+
+export default router;
