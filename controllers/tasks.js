98adaf5c120361d7d2d1f95ba6f8ae8b6758da6a
Jason Zhao <jason@goopter.com>
2022-10-16 00:36:10 -0700

    changed file name and added function shells

---
+++ controllers/tasks.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+import PostMessage from '../models/task.js';
+
+const router = express.Router();
+
+export const getTasks = async (req, res) => { 
+}
+
+export const getTask = async (req, res) => { 
+}
+
+export const createTask = async (req, res) => {
+}
+
+export const updateTask = async (req, res) => {
+}
+
+export const deleteTask = async (req, res) => {
+}
+
+export default router;
